acefa014ca69e3801cbaeeebe1557331636d4205
beatrizzpaulinaa <beatrizzpaulinaa@gmail.com>
2026-05-20 10:16:09 -0300

    estrutura de dados

---
+++ Desafio01/script.js
@@ -0,0 +1,14 @@
+const alunos = [
+        {nome: "Gabriella", idade: 17, disciplinaPreferida: "Matemática"},
+        {nome: "Stefany", idade: 17, disciplinaPreferida: "Física"},
+        {nome: "Nathalia", idade: 16, disciplinaPreferida: "Química"},
+        {nome: "João Victor", idade: 17, disciplinaPreferida: "Biologia"},
+        {nome: "Luan", idade: 15, disciplinaPreferida: "Língua Portuguesa"}
+];
+
+const primeiroNome = alunos [0].nome;
+const ultimaDisciplina = alunos [4].disciplinaPreferida;
+
+alert("Nome do primeiro aluno:" + primeiroNome);
+alert("Disciplina do último aluno:" + ultimaDisciplina);
+
